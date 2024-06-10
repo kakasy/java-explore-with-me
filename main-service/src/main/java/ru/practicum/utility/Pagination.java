86c9a10cf13b9cb05e102720ff6580ca8b7e460c
kakasy <drum2to@yandex.ru>
2024-06-10 18:21:05 +0300

    feat: добавить сортировку

--- main-service/src/main/java/ru/practicum/utility/Pagination.java
+++ main-service/src/main/java/ru/practicum/utility/Pagination.java
@@ -3,6 +3,7 @@ package ru.practicum.utility;
 import lombok.experimental.UtilityClass;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @UtilityClass
 public class Pagination {
@@ -10,5 +11,10 @@ public class Pagination {
     public static Pageable withoutSort(Integer from, Integer size) {
 
         return PageRequest.of(from / size, size);
+    }
+
+    public static Pageable withSort(Integer from, Integer size, Sort sort) {
+
+        return PageRequest.of(from / size, size, sort);
     }
 }
